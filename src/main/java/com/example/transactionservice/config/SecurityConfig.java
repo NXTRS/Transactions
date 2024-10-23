c88f9492519a8ba9bc05eba0542e8f6e292ab5e2
lucianvaida <lucian.vaida.ext@ausy-technologies.de>
2024-10-23 21:16:16 +0300

    added security configuration

--- src/main/java/com/example/transactionservice/config/SecurityConfig.java
+++ src/main/java/com/example/transactionservice/config/SecurityConfig.java
@@ -7,12 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 @EnableWebSecurity
